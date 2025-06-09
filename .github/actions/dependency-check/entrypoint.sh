8202c22d3f9824e04560d1413cb11d5f8347fccb
diego4bits <djv.benavides@proton.me>
2025-06-09 10:06:27 -0500

    initial commit

--- .github/actions/dependency-check/entrypoint.sh
+++ .github/actions/dependency-check/entrypoint.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# --- SOLUCIÓN AUTOCONTENIDA ---
-# Usando la ruta que descubriste ('/opt/jdk'), añadimos su directorio 'bin'
-# al principio de la variable de entorno PATH.
-# Esto asegura que cuando cualquier script busque el comando "java", encontrará
-# la versión correcta dentro del contenedor primero, solucionando el problema
-# de la sobrescritura de JAVA_HOME de forma definitiva.
+# We need to set JAVA_HOME because thhe Setup JDK action overwrites the correct path for the container. 
 export JAVA_HOME="/opt/jdk/"
-# --- FIN DE LA SOLUCIÓN ---
 
-
-# El resto de tu script original continúa sin cambios.
 scan_args="$1"
 out_path="/github/workspace"
 
-# El siguiente comando ahora usará la versión correcta de Java gracias al PATH modificado
 /usr/share/dependency-check/bin/dependency-check.sh \
       $scan_args \
       --out $out_path
